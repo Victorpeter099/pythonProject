9a9b85bb89a3ba471bac4ae58ba24e435f2aa6a2
Victorpeter099 <122839308+Victorpeter099@users.noreply.github.com>
2023-07-18 12:27:32 +0100

    my first experience with functions in python.

---
+++ funtion.py
@@ -0,0 +1,8 @@
+#First experience with functions in python.
+def say_hi(name, age, time,job):
+    print("hello " + name + ",i am " + age + " i live at 267 Avenue cleveland ohio, i leave my house\n  "
+                                             " to work at exactly " + time + " i love my work as a " + job)
+
+
+say_hi("Victor", "30", "8:00am", "python programmer")
+say_hi("Peter", "40", "8:00am", "python programmer")
